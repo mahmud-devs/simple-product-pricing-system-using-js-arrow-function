32facd19c8a89dcab57efaba7266b8d23840921c
mahmud-devs <mahmudulislam0999@gmail.com>
2024-02-07 21:33:43 +0600

    all done

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*
+
+=== product pricing system using javascript function ===
+
+*/
+
+const originalPrice = (unitPrice, unitPriduct) => {
+    let priceOriginal = unitPrice * unitPriduct;
+    return priceOriginal;
+};
+
+const priceWithVat = (returnPriceOriginal, vat) => {
+    let vatAddedPrice =
+        (returnPriceOriginal * vat) / (100 + vat) + returnPriceOriginal;
+    return Math.ceil(vatAddedPrice);
+};
+
+const finalPrice = (returnVatAddedPrice, discount) => {
+    let priceFinal =
+        returnVatAddedPrice - (returnVatAddedPrice * discount) / 100;
+    return Math.ceil(priceFinal);
+};
+
+// ============ perameters =============
+
+let price = prompt("Enter your price range:");
+let unitPrice = Number(price);
+
+let product = prompt("Enter product quantity:");
+let unitPriduct = parseInt(product);
+
+let vat = 15;
+
+let discount = 5;
+// ============== invoking original price function ===============
+
+returnPriceOriginal = originalPrice(unitPrice, unitPriduct);
+
+// ============== invoking price with vat function function ===============
+
+returnVatAddedPrice = priceWithVat(returnPriceOriginal, vat);
+
+// ================ invoking vat added price with discount function =========
+
+returnPriceFinal = finalPrice(returnVatAddedPrice, discount);
+
+// ================ outputs =====================
+
+console.log(
+    "original price without any extra charge:",
+    returnPriceOriginal,
+    "taka"
+);
+console.log("Total price with vat:", returnVatAddedPrice, "taka");
+console.log("Final price with discount:", returnPriceFinal, "taka");
